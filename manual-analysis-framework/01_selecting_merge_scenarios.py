7f1cc5fe5933b867127214765364528cc00dc87b
Gustavo Vale <vale@cs.uni-saarland.de>
2023-06-22 14:41:44 -0300

    Refactoring the selection of sample scenarios

--- manual-analysis-framework/01_selecting_merge_scenarios.py
+++ manual-analysis-framework/01_selecting_merge_scenarios.py
@@ -2,14 +2,19 @@ import pandas as pd
 
 df = pd.read_csv(r'../raw-input-data/ms-data.csv')
 
-# Getting sample for conflicting scenarios
-conflicting_ms = df.loc[df['has_conflict'] == 1];
-conflicting_ms_sample =conflicting_ms.sample(n = 200)
-conflicting_ms_sample.to_csv('data/01_conflicting_ms_sample.csv')
-
-# Getting sample for safe scenarios
-safe_ms = df.loc[df['has_conflict'] == 0];
-safe_ms_sample = safe_ms.sample(n = 200)
-safe_ms_sample.to_csv('data/01_safe_ms_sample.csv')
+# Save sample of dataframe in csv with or without conflicts
 
 
+def save_sample_df(df, sample_size, is_conflicting, filepath):
+    new_df = df.loc[df['has_conflict'] == is_conflicting]
+    new_df = new_df.sample(n=sample_size)
+    new_df.to_csv(filepath)
+
+
+# For conflicting scenarios
+conflicting_ms_sample = save_sample_df(
+    df, 200, 1, 'data/01_conflicting_ms_sample.csv')
+
+# For safe scenarios
+conflicting_ms_sample = save_sample_df(
+    df, 200, 0, 'data/01_safe_ms_sample.csv')
